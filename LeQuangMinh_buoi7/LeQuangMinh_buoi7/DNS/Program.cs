5d5bbb84b88bd0d075efba79972c82e01043d05f
jick lee <jickleesqk@gmail.com>
2017-11-26 22:14:32 +0700

    Buoi 7 fixed

--- LeQuangMinh_buoi7/LeQuangMinh_buoi7/DNS/Program.cs
+++ LeQuangMinh_buoi7/LeQuangMinh_buoi7/DNS/Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace DNS
 {
     class Program
     {
         static void Main(string[] args)
+
         {
+            Console.Write("Nhap ten mien hoac ip: ");
+            string host = Console.ReadLine();
+            IPHostEntry e = Dns.GetHostEntry(host);
+            Console.WriteLine("Hostname: {0}", e.HostName);
+            foreach (string s in e.Aliases)
+                Console.WriteLine("\t+ Alias: {0} \n", s);
+            foreach (IPAddress i in e.AddressList)
+                Console.WriteLine("\t+ IP: {0}", i);
+            Console.ReadLine();
+
         }
     }
 }
